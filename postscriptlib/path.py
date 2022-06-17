638fc8bc65ea7233f4f16b411aa60409dc8f99c0
Peter Gagliardi <ptrgags@gmail.com>
2022-06-17 16:16:01 -0400

    Convert fm_ring to sketchbook format

--- postscriptlib/path.py
+++ postscriptlib/path.py
@@ -12,12 +12,24 @@ class Path:
         self.lines.append(f"0 {h:.4f} rlineto")
         self.lines.append(f"{-w:.4f} 0 rlineto")
         self.lines.append(f"0 {-h:.4f} rlineto")
+
+    def polygon(self, vertices):
+        for i, (x, y) in enumerate(vertices):
+            if i == 0:
+                self.lines.append(f"{x:.4f} {y:.4f} moveto")
+            else:
+                self.lines.append(f"{x:.4f} {y:.4f} lineto")
+        (x, y) = vertices[-1]
+        self.lines.append(f"{x:.4f} {y:.4f} lineto")
     
     def rpolygon(self, origin, displacements):
         (ox, oy) = origin
         self.lines.append(f"{ox:.4f} {oy:.4f} moveto")
         for (x, y) in displacements:
             self.lines.append(f"{x:.4f} {y:.4f} rlineto")
+
+    def close(self):
+        self.lines.append("closepath")
 
     def to_postscript(self):
         return ["newpath"] + self.lines
