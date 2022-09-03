919570a55796fe876aa1500b20e7995fed31ad82
Peter Gagliardi <ptrgags@gmail.com>
2022-09-03 18:37:39 -0400

    Handle the edges

--- artworks/edge_directions.py
+++ artworks/edge_directions.py
@@ -24,7 +24,7 @@ EDGE = 2
 TILE_CHOICES = {
     # (right, up, left, down) -> set((tile_type, rotation))
     # where rotation is the number of 90 degree CCW rotations to apply
-    (IN, IN, IN, IN): {("sink", 0)},
+    (IN, IN, IN, IN): {("sink4", 0)},
     (IN, IN, IN, OUT): {("fork3", 0)},
     (IN, IN, OUT, IN): {("fork3", 3)},
     (IN, IN, OUT, OUT): {("truchet", 0), ("cross", 0)},
@@ -39,8 +39,41 @@ TILE_CHOICES = {
     (OUT, OUT, IN, IN): {("truchet", 0), ("cross", 0)},
     (OUT, OUT, IN, OUT): {("fork3", 3)},
     (OUT, OUT, OUT, IN): {("fork3", 0)},
-    (OUT, OUT, OUT, OUT): {("source", 0)},
+    (OUT, OUT, OUT, OUT): {("source4", 0)},
 }
+
+# For edges, there are quite a few cases. To keep it simple, let's rotate
+# the first edge so it's at the bottom. If there are 2 edges (corner), assume
+# the second edge is on the right.
+#
+# In all 12 cases, there is only one choice, and the rotation is handled
+# when rotating the boundary to the bottom of the tile, so this dictionary
+# is simpler in a way
+EDGE_TILE_CHOICES = {
+    # (right, up, left) -> tile_type
+    (IN, IN, IN): "sink3",
+    (IN, IN, OUT): "l_fork2",
+    (IN, OUT, IN): "fork2",
+    (IN, OUT, OUT): "r_fork2",
+    (OUT, IN, IN): "r_fork2",
+    (OUT, IN, OUT): "fork2",
+    (OUT, OUT, IN): "l_fork2",
+    (OUT, OUT, OUT): "source3",
+    (EDGE, IN, IN): "sink2",
+    (EDGE, IN, OUT): "corner",
+    (EDGE, OUT, IN): "corner",
+    (EDGE, OUT, OUT): "source2",
+}
+
+def left_shift_tuple4(tuple, places):
+    return (
+        tuple[places % 4],
+        tuple[(1 + places) % 4],
+        tuple[(2 + places) % 4],
+        tuple[(3 + places) % 4]
+    )
+
+assert left_shift_tuple4((1, 2, 3, 4), 3) == (4, 1, 2, 3)
 
 
 class EdgeDirectionTiling(receipts.Receipt):
@@ -87,9 +120,6 @@ class EdgeDirectionTiling(receipts.Receipt):
         for i in range(1, (self.grid_height + 1) - 1):
             for j in range(self.grid_width):
                 self.h_edges[i][j] = random.choice(H_CHOICES)
-        
-        print(self.v_edges)
-        print(self.h_edges)
     
     def populate_tiles(self):
         for i in range(self.grid_height):
@@ -143,14 +173,52 @@ class EdgeDirectionTiling(receipts.Receipt):
         tile_choices = TILE_CHOICES.get(key, None)
 
         if tile_choices is None:
-            return None
+            return self.pick_edge_tile(key)
         
         return random.choice(tuple(tile_choices))
+    
+    def pick_edge_tile(self, key):
+        # 4 edges to a tile
+        N = 4
+
+        # Find the first edge. This will always be defined
+        first_edge = None
+        for i in range(N):
+            if key[i] == EDGE:
+                first_edge = i
+                break
+        
+        # Find the second edge. If it exists, this is a corner tile
+        second_edge = None
+        for i in range(first_edge + 1, N):
+            if key[i] == EDGE:
+                second_edge = i
+                break
+
+        # Determine which edge to put at the bottom. For corners, we want
+        # the second edge to be on the right
+        start_index = first_edge
+        if second_edge is not None and (second_edge - first_edge) != 1:
+            start_index = second_edge
+        
+        # How many 90 degree rotations are needed to rotate from the bottom
+        # to the correct orientation.
+        rotation = (start_index + 1) % N
+
+        # cycle the key so that the edge is in the first slot
+        # e.g. (IN, EDGE, OUT, OUT) -> (EDGE, OUT, OUT, IN). This is
+        # symbolically like rotating the tile so that the edge is on the bottom.
+        edge_key = left_shift_tuple4(key, start_index)
+
+        # The lookup table assumes the first entry is EDGE since it's always
+        # on the bottom
+        tile = EDGE_TILE_CHOICES[edge_key[1:]]
+        return (tile, rotation)
     
     def draw(self):
         thickness = 1.0 / self.square_size
 
-        self.define_function("sink", [
+        self.define_function("sink4", [
             "newpath",
             "0 0.5 moveto",
             "1 0.5 lineto",
@@ -167,7 +235,7 @@ class EdgeDirectionTiling(receipts.Receipt):
             "stroke"
         ])
 
-        self.define_function("source", [
+        self.define_function("source4", [
             "newpath",
             "0 0.5 moveto",
             "1 0.5 lineto",
@@ -212,6 +280,97 @@ class EdgeDirectionTiling(receipts.Receipt):
             "stroke"
         ])
 
+        self.define_function("corner", [
+            "newpath",
+            "0 1 0.5 270 360 arc",
+            "stroke",
+        ])
+
+        self.define_function("fork2", [
+            "newpath",
+            "0 1 0.5 270 360 arc",
+            "stroke",
+            "newpath",
+            "1 1 0.5 180 270 arc",
+            "stroke"
+        ])
+
+        self.define_function("l_fork2", [
+            "newpath",
+            "0 1 0.5 270 360 arc",
+            "stroke",
+            "newpath",
+            "0 0.5 moveto", 
+            "1 0.5 lineto",
+            "stroke"
+        ])
+
+        self.define_function("r_fork2", [
+            "newpath",
+            "1 1 0.5 180 270 arc",
+            "stroke",
+            "newpath",
+            "0 0.5 moveto", 
+            "1 0.5 lineto",
+            "stroke"
+        ])
+
+        self.define_function("sink2", [
+            "newpath",
+            "0 0.5 moveto",
+            "0.5 0.5 lineto",
+            "0.5 1 lineto",
+            "stroke",
+            "newpath",
+            "0.5 0.5 0.125 0 360 arc",
+            "gsave",
+            "1 setgray",
+            "fill",
+            "grestore",
+            "0 setgray",
+            "stroke"
+        ])
+
+        self.define_function("sink3", [
+            "newpath",
+            "0 0.5 moveto",
+            "1 0.5 lineto",
+            "0.5 0.5 moveto",
+            "0.5 1 lineto",
+            "stroke",
+            "newpath",
+            "0.5 0.5 0.125 0 360 arc",
+            "gsave",
+            "1 setgray",
+            "fill",
+            "grestore",
+            "0 setgray",
+            "stroke"
+        ])
+
+        self.define_function("source2", [
+            "newpath",
+            "0 0.5 moveto",
+            "0.5 0.5 lineto",
+            "0.5 1 lineto",
+            "stroke",
+            "newpath",
+            "0.5 0.5 0.125 0 360 arc",
+            "fill",
+        ])
+
+        self.define_function("source3", [
+            "newpath",
+            "0 0.5 moveto",
+            "1 0.5 lineto",
+            "0.5 0.5 moveto",
+            "0.5 1 lineto",
+            "stroke",
+            "newpath",
+            "0.5 0.5 0.125 0 360 arc",
+            "fill",
+        ])
+
         self.define_function("rot90", [
             "0.5 0.5 translate",
             "90 mul rotate",
@@ -219,6 +378,7 @@ class EdgeDirectionTiling(receipts.Receipt):
         ])
 
         self.add_lines([
+            f"0 0 {self.width} {self.height} rectstroke",
             f"{self.square_size} {self.square_size} scale",
             f"{thickness} setlinewidth",
         ])
