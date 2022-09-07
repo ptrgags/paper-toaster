b76140c39bce7ce423ce53cadae7a3f3c2d69751
Peter Gagliardi <ptrgags@gmail.com>
2022-09-06 20:32:22 -0400

    Add braid grouping

--- artworks/colored_braids.py
+++ artworks/colored_braids.py
@@ -25,7 +25,15 @@ GRAY_VALUES_NORMALIZED = [float(i) / (GRAY_N - 1) for i in range(GRAY_N)]
 GRAY_VALUES = [0.5 * x + 0.25 for x in GRAY_VALUES_NORMALIZED]
 PALETTE_GRAYSCALE = [f"{x} {x} {x}" for x in GRAY_VALUES]
 
-PALETTE = PALETTE_RAINBOW
+PALETTE_STRIPES = [
+    "1 0 0",
+    "0.8 0.8 0.8",
+    "1 1 1",
+    "0.5 0.5 0.5",
+    "0.5 0.5 0.5"
+]
+
+PALETTE = PALETTE_STRIPES
 PALETTE_SIZE = len(PALETTE)
 
 def odd_even_shuffle(array, rows, swap_chance):
@@ -76,6 +84,29 @@ def odd_even_sort(array):
         yield row_commands
         sort_pass += 1
 
+def strand_groups(s):
+    return [int(x) for x in s.split(",")]
+
+def make_groups(group_lengths, total_strands):
+    all_strands = list(range(total_strands))
+
+    strand_index = 0
+    length_index = 0
+    result = []
+    while strand_index < total_strands:
+        group_length = group_lengths[length_index]
+        group = all_strands[strand_index:strand_index + group_length]
+        result.append(group)
+
+        # The slice might be smaller than group_length if we're at the end
+        strand_index += len(group)
+
+        # If group_lengths is too short, cycle around
+        length_index += 1
+        length_index %= len(group_lengths)
+    
+    return result
+
 class ColoredBraids(receipts.Receipt):
     ARTWORK_ID = 'colored_braids'
     
@@ -85,7 +116,7 @@ class ColoredBraids(receipts.Receipt):
             '-s',
             '--square-size',
             type=float,
-            default=0.25,
+            default=0.125,
             help="Size of a grid square in inches. Defaults to 1/4 inch"
         )
         subparser.add_argument(
@@ -93,13 +124,26 @@ class ColoredBraids(receipts.Receipt):
             '--swap-chance',
             type=float,
             default=0.5,
-            help="Chance of swapping a braid strand with its neighbor as a number in [0.0, 1.0]"
+            help=(
+                "Chance of swapping a braid strand with its neighbor as a "
+                "number in [0.0, 1.0]"
+            )
         )
         subparser.add_argument(
             '-i',
             '--invert-colors',
             action="store_true",
             help="If set, use an inverted color scheme"
+        )
+        subparser.add_argument(
+            '-g',
+            '--groups',
+            type=strand_groups,
+            help=(
+                "CSV of positive integers that determine groups of strands to "
+                "weave. E.g. 3,4 means weave the first 3 strands, and the next "
+                "4 strands separately. If not specified, all strands are woven."
+            )
         )
 
     def setup(self):
@@ -109,6 +153,10 @@ class ColoredBraids(receipts.Receipt):
         self.square_size = square_size
         self.invert_colors = self.args.invert_colors
         self.swap_chance = self.args.swap_chance
+
+        num_strands = self.grid_width
+        self.groups = self.args.groups or [num_strands]
+        self.strand_array = make_groups(self.groups, num_strands)
 
     def draw(self):
         bg_color = 0 if self.invert_colors else 1
@@ -202,28 +250,34 @@ class ColoredBraids(receipts.Receipt):
             f"0.5 0 translate"
         ])
 
-        # strands are on the edges of the grid squares
-        strands = self.grid_width
-        rows = self.grid_height
-        strand_array = list(range(strands))
+        start_column = 0
+        for group in self.strand_array:
+            self.draw_group(start_column, group)
+            start_column += len(group)
         
+    def draw_group(self, start_column, group):
         # Draw the strands straight for the first row
-        for i, strand in enumerate(strand_array):
+        for i, strand in enumerate(group):
+            x = start_column + i
             color = PALETTE[strand % PALETTE_SIZE]
             self.add_lines([
                 "gsave",
-                f"{i} 0 translate",
+                f"{x} 0 translate",
                 f"{color} straight1",
                 "grestore"
             ])
         
+
+        num_rows = self.grid_height - 1
+        
         # Progressively swap strands and add draw the result
-        shuffle_gen = odd_even_shuffle(strand_array, rows - 1, self.swap_chance)
+        shuffle_gen = odd_even_shuffle(group, num_rows, self.swap_chance)
         for i, commands in enumerate(shuffle_gen):
             y = i + 1
             for (command, *args) in commands:
                 if command == STRAIGHT1:
                     x, strand = args
+                    x += start_column
                     color = PALETTE[strand % PALETTE_SIZE]
                     self.add_lines([
                         "gsave",
@@ -233,6 +287,7 @@ class ColoredBraids(receipts.Receipt):
                     ])
                 elif command == STRAIGHT2:
                     x, strand1, strand2 = args
+                    x += start_column
                     left_color = PALETTE[strand1 % PALETTE_SIZE]
                     right_color = PALETTE[strand2 % PALETTE_SIZE]
                     self.add_lines([
@@ -243,6 +298,7 @@ class ColoredBraids(receipts.Receipt):
                     ])
                 elif command == CROSS:
                     x, strand1, strand2 = args
+                    x += start_column
                     left_color = PALETTE[strand1 % PALETTE_SIZE]
                     right_color = PALETTE[strand2 % PALETTE_SIZE]
                     if x % 2 == 0:
