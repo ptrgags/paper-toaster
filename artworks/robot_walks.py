6e7a636cc930a3b4553d34cc72e999d9c9338f06
Peter Gagliardi <ptrgags@gmail.com>
2024-06-10 10:36:24 -0400

    Make a simple data structure for arcs

--- artworks/robot_walks.py
+++ artworks/robot_walks.py
@@ -16,6 +16,7 @@ from postscriptlib.vec2 import Vec2
 N = 5
 TAU = 2.0 * math.pi
 TAU_DEGREES = 360
+FIFTH_TURN_DEGREES = TAU_DEGREES / N
 ROOTS = [
     Vec2.direction_vec(TAU * i / N)
     for i in range(N)
@@ -80,6 +81,38 @@ R = RobotCommand.right()
 L = RobotCommand.left()
 
 
+class RobotArc:
+    """
+    A simple struct for generating the PostScript arc or arcn commands
+    for the robot arcs. These are always 1/5 turns
+    """
+    FIFTH_TURN = TAU_DEGREES / 5
+
+    def __init__(self, center, start_angle, end_angle, clockwise):
+        self.center = center
+
+        if clockwise:
+            # the arcn command is weird and measures angles clockwise
+            self.start_angle = -(180 - start_angle * FIFTH_TURN_DEGREES)
+            self.end_angle = -(180 - end_angle * FIFTH_TURN_DEGREES)
+            self.arc_command = 'arcn'
+        else:
+            self.start_angle = start_angle * FIFTH_TURN_DEGREES
+            self.end_angle = end_angle * FIFTH_TURN_DEGREES
+            self.arc_command = 'arc'
+
+    def __str__(self):
+        """
+        Generate the PostScript command
+        """
+        x = self.center.x
+        y = self.center.y
+        start = self.start_angle
+        end = self.end_angle
+        cmd = self.arc_command
+        return f'{x} {y} 1 {start} {end} {cmd}'
+
+
 def generate_commands(length):
     choices = [R, L]
     commands = []
@@ -102,8 +135,10 @@ class RobotWalks(receipts.Receipt):
         print([x.label for x in self.commands])
 
     def draw(self):
+        if self.args.seed:
+            random.seed(self.args.seed)
+
         vertices = [Vec2(0.0, 0.0)]
-        centers = []
         arcs = []
 
         robot_path = RobotCommand.identity()
@@ -121,10 +156,17 @@ class RobotWalks(receipts.Receipt):
                     0.5 * -offset +
                     0.5 * OFFSET_MAGNITUDE * 1.0 / math.tan(TAU / 10) * left
                 )
-                centers.append(center)
-                end_angle = robot_path.orientation * TAU_DEGREES / 5
-                start_angle = prev_orientation * TAU_DEGREES / 5
-                arcs.append((center, start_angle, end_angle, 'arc'))
+
+                arcs.append(RobotArc(
+                    center,
+                    prev_orientation,
+                    robot_path.orientation,
+                    False
+                ))
+
+                # end_angle = robot_path.orientation * TAU_DEGREES / 5
+                # start_angle = prev_orientation * TAU_DEGREES / 5
+                # arcs.append((center, start_angle, end_angle, 'arc'))
             else:
                 prev_orientation = (robot_path.orientation + 1) % N
                 offset = OFFSETS[robot_path.orientation]
@@ -134,12 +176,17 @@ class RobotWalks(receipts.Receipt):
                     0.5 * -offset +
                     0.5 * OFFSET_MAGNITUDE * 1.0 / math.tan(TAU / 10) * right
                 )
-                centers.append(center)
 
-                end_angle = 180 - robot_path.orientation * TAU_DEGREES / 5
-                start_angle = 180 - prev_orientation * TAU_DEGREES / 5
+                # end_angle = 180 - robot_path.orientation * TAU_DEGREES / 5
+                # start_angle = 180 - prev_orientation * TAU_DEGREES / 5
                 # arcn is weird and measures angles clockwise
-                arcs.append((center, -start_angle, -end_angle, 'arcn'))
+                # arcs.append((center, -start_angle, -end_angle, 'arcn'))
+                arcs.append(RobotArc(
+                    center,
+                    prev_orientation,
+                    robot_path.orientation,
+                    True
+                ))
 
         self.add_lines([
             f"{self.width / 2} {self.height / 2} translate",
@@ -147,22 +194,19 @@ class RobotWalks(receipts.Receipt):
             "0.005 setlinewidth",
         ])
 
-        # lines = path.Path()
-        # lines.polygon(vertices)
-        # self.add_path(lines)
-        # self.add_lines(["0.005 setlinewidth stroke"])
-
         self.add_lines(["newpath"])
-        for center, start, end, ps_command in arcs:
-            print(center, start, end, ps_command)
+        self.add_lines([str(arc) for arc in arcs])
+
+        """
+        for center, start, end, arc_command in arcs:
+            print(center, start, end, arc_command)
             self.add_lines([
-                f"{center.x} {center.y} 1 {start} {end} {ps_command}",
+                f"{center.x} {center.y} 1 {start} {end} {arc_command}",
             ])
+        """
         self.add_lines(["stroke"])
 
         dots = path.Path()
         for vertex in vertices:
             dots.circle(vertex, 0.04)
-        # for center in centers:
-        #    dots.circle(center, 0.04)
         self.add_path(dots)
